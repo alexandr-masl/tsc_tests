364a633a90a1fb7f0db1ffc33d0a7206059713dd
Ken-Dra <for.shit@yahoo.com>
2021-11-16 15:56:54 +0300

    websocket

--- test.ts
+++ test.ts
@@ -1,5 +1,40 @@
+const { tokens: { ApiKey } } = require('../settings.json');
+const { tokens: { SecretKey } } = require('../settings.json');
+const Binance = require('binance-api-node').default;
+import { BinanceExch } from './Exchange/Binance';
+import {Exchange} from './Exchange/Exchange';
+const ws  = require('ws')
+
+const TestClient = new BinanceExch(ApiKey, SecretKey)
 
 
-const Telegraf = require('telegraf');
+const client = Binance({
+    apiKey: ApiKey,
+    apiSecret: SecretKey,
+  })
+  
+  console.log(client.ping())
+  console.log(client.exchangeInfo())
+  console.log(client.getInfo())
 
-this.tg_bot_inst = new Telegraf(this.token);
+const socket = new ws("wss://stream.binance.com:9443/ws/ltcbtc@aggTrade/ethbtc@aggTrade")
+socket.onopen = function(e) {
+   console.log("Соединение установлено");
+   console.log("Отправляем данные на сервер");
+};
+  
+  socket.onmessage = function(event) {
+    console.log(`Данные получены с сервера: ${event.data}`);
+  };
+  
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+        console.log(`Соединение закрыто, код=${event.code} причина=${event.reason}`);
+    } else {
+        console.log('Соединение прервано');
+    }
+  };
+   socket.onerror = function(error) {
+       console.log(`[error] ${error.message}`);
+  };
+
